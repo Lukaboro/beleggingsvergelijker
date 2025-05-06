7ee815295646cd2fb6110bafc32567aab4c5e52f
Pieter De Knock <deknock@hotmail.com>
2025-05-06 11:53:57 +0200

    Fix React build issues with corrected index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Zonder de index.css import
 import App from './App';
-import './index.css'; // als je Tailwind of andere CSS gebruikt
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

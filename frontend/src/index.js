4e50b2e124d62524eba19fe32263eb77525e8d9a
Pieter De Knock <deknock@hotmail.com>
2025-05-06 11:29:42 +0200

    Voeg public map en index.html toe, plus package.json

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // als je Tailwind of andere CSS gebruikt
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
